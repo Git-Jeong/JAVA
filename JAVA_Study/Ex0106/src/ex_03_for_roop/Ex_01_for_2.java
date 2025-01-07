a4529ae36fe65e6ad8712d64841aadb3720ee8db
JeongJin <jeongjin9427@gmail.com>
2025-01-07 16:38:19 +0900

    Update Ex_01_for_2.java

--- JAVA_Study/Ex0106/src/ex_03_for_roop/Ex_01_for_2.java
+++ JAVA_Study/Ex0106/src/ex_03_for_roop/Ex_01_for_2.java
@@ -1,18 +1,28 @@
 package ex_03_for_roop;
 
+import java.util.Scanner;
+
 public class Ex_01_for_2 {
 	public static void main(String[] args) {
-		// 구구단을 원하는 만큼만 출력하는 코드
+		// 구구단을 원하는 만큼만 출력하는 코드 
 		
-		final int START_1 = 2;
-		final int START_2 = 1;
-		final int MAX_1 = 2;
-		final int MAX_2 = 9;
+		Scanner scan = new Scanner(System.in);
+		int input;
 		
-		for(int i = START_1; i <= MAX_1; i++) {
-			for(int j = START_2; j <= MAX_2; j++) {
-				System.out.printf("%d x %d = %d\n", i, j, (i*j));
+		System.out.print("단을 입력해 주세요 : ");
+		while(true) {
+			try {
+				input = Integer.parseInt(scan.nextLine());
+				break;
+			}catch (Exception e) {
+				System.out.print("옳바른 숫자를 다시 입력해 주세요 : ");
 			}
 		}
+		
+		for(int i = 1; i <= 9; i++) {
+			System.out.printf("%d x %d = %d\n", input, i, (input*i));
+		}
+		
+		scan.close(); 
 	}
 }
