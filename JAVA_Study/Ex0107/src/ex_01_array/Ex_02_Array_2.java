ba95d57201acc6397fdeac3d8c082debdc86fec7
JeongJin <jeongjin9427@gmail.com>
2025-01-08 16:41:09 +0900

    Update Ex_02_Array_2.java
    ArrayList<Integer> 정수형_배열의_이름 = new ArrayList<>();

--- JAVA_Study/Ex0107/src/ex_01_array/Ex_02_Array_2.java
+++ JAVA_Study/Ex0107/src/ex_01_array/Ex_02_Array_2.java
@@ -1,31 +1,33 @@
 package ex_01_array;
 
 import java.util.Random;
+import java.util.ArrayList;
 
 public class Ex_02_Array_2 {
 	public static void main(String[] args) {
-		// 정수형 데이터 5개를 저장할 수 있는 배열 array를 하나 선언
-		// 배열 안의 모든 데이터를 임의의 값으로 초기화(1~100)
-		// 배열 안의 데이터 중 홀수의 값만 출력하고, 총 홓수가 몇 개 인지 출력
-		
+		// 임의의 값(1~100)을 5개 불러오기 
+		// 홀수만 가변 배열에 저장
+		// 가변 배열의 값을 출력하고, 총 홓수가 몇 개 인지 출력
+
 		Random rd = new Random();
 		final int MAX_ARRAY_INDEX = 5;
 		final int MAX_RANDOM_NUM = 100;
-		
-		int rd_array[] = new int[MAX_ARRAY_INDEX];
-		int count = 0;
-		
+ 
+		ArrayList<Integer> oddNumbers = new ArrayList<>(); // 홀수를 담을 가변 배열
+
 		System.out.printf("배열 array에 들어 있는 홀수는 ");
-		for(int i=0; i < MAX_ARRAY_INDEX; i++) {
-			rd_array[i] = rd.nextInt(MAX_RANDOM_NUM) + 1; 
-			if(rd_array[i] % 2 == 1) {
-				System.out.printf("%d ", rd_array[i]);
-				count++;
+		for (int i = 0; i < MAX_ARRAY_INDEX; i++) {
+			int temp = rd.nextInt(MAX_RANDOM_NUM) + 1;
+			if (temp % 2 == 1) {
+				oddNumbers.add(temp); // 홀수만 추가 
 			}
 		}
+  
+		for (int temp : oddNumbers) {
+		    System.out.printf("%d ", temp);
+		}
 		
-		System.out.println("이며, ");
-		System.out.printf("총 %d개 입니다. ", count);
-
+		System.out.println();
+		System.out.printf("총 %d개 입니다. ", oddNumbers.size());
 	}
 }
