5ac9ce1ea88b46c843bc06e6bbd4f048a07f82a3
JeongJin <jeongjin9427@gmail.com>
2025-01-31 15:49:28 +0900

    Update Ex_01_main.java

--- JAVA_Study/RegularEmployee/src/ex_interfate/Ex_01_main.java
+++ JAVA_Study/RegularEmployee/src/ex_interfate/Ex_01_main.java
@@ -1,5 +1,16 @@
-package ex_interfate;
+package ex_interfate; 
 
 public class Ex_01_main {
-
+	public static void main(String[] args) {
+		System.out.println("시작");
+		new Temp() {
+			
+			@Override
+			public void temp() {
+				System.out.println(NUM);
+			}
+		}.temp();
+		//.temp를 적어야 속에있는 temp 메소드를 실행한다!
+		 
+	}
 }
